e00ef6caded993880b4184e1f3b450a8af2cfdaf
Erik Staats <erik.staats@gmail.com>
2016-01-15 21:51:47 -0800

    Add guess number validation.

--- src/guess.js
+++ src/guess.js
@@ -6,6 +6,23 @@
  *******************************************************************************
  ******************************************************************************/
 
+/*******************************************************************************
+ *
+ * Guess My Number game globals.
+ *
+ *   min_number             Minimum guess number.
+ *   max_number             Maximum guess number.
+ */
+
+var min_number = 1;
+var max_number = 10;
+
+
+/*******************************************************************************
+ *
+ * Guess My Number game functions.
+ */
+
 /*
  * Handle the guess form submission event specified by event.
  *
@@ -19,10 +36,19 @@ function handleGuess(event)
 
     /* Get the player's guess and clear it. */
     var guess_input = document.getElementById("guess_input");
-    var guess_val = guess_input.value;
+    var guess_val = parseInt(guess_input.value);
     guess_input.value = "";
 
     /* Display the player's guess. */
     var players_guess = document.getElementById("players_guess");
-    players_guess.innerHTML = "Your guess is " + guess_val.toString();
+    if ((guess_val >= min_number) && (guess_val <= max_number))
+    {
+        players_guess.innerHTML = "Your guess is " + guess_val.toString() + ".";
+    }
+    else
+    {
+        players_guess.innerHTML = "Please guess a number between " +
+                                  min_number.toString() + " and " +
+                                  max_number.toString() + ".";
+    }
 }
